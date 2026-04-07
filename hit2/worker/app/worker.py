58f3d8eec27ac8e425a5468d09271e8c98a10dda
Mausqui <ausquim@gmail.com>
2026-04-06 23:17:50 -0300

    bases de hit2 faltan readme y timestamps

---
+++ hit2/worker/app/worker.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from collections import Counter
+import re
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Texto(BaseModel):
+    cuerpo_texto: str
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/health")
+def get_ping():
+    return {"status": "ok"}
+
+
+@app.put("/contar_palabras")
+def contar_palabras(texto: Texto):
+    tratado: str = re.sub(r'[^a-zA-ZáéíóúÁÉÍÓÚñÑ]', ' ', texto.cuerpo_texto)
+    tratado = tratado.lower()
+    palabras = tratado.split()
+    ocurrencias_palabras = Counter(palabras)
+    return {"ocurrencias": ocurrencias_palabras}
